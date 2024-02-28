3e7ab160f696f6ebf54241dadc70aa7c35a402b6
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2024-02-28 16:51:13 +0600

    Carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import HeroCarousel from '@/components/HeroCarousel'
 import SearchBar from '@/components/SearchBar'
 import Image from 'next/image'
 import React from 'react'
@@ -29,7 +30,7 @@ const Home = () => {
             <SearchBar/>
           </div>
 
-          HeroCarousel
+          <HeroCarousel/>
         </div>
       </section>
       <section className='trending-section'>
