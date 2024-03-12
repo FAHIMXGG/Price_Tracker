30b48d004988bf05d198387542d3d540f75d966e
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2024-03-12 15:15:11 +0600

    test

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,14 +1,19 @@
 "use client"
 
-const SearchBar = () => {
-    const handleSubmit = () =>{
+import { useState } from "react";
 
+const SearchBar = () => {
+    const [searchPrompt, setSearchPrompt] = useState('')
+    const handleSubmit = () =>{
+        
     }
     return (
         <form action="" className='flex flex-warp gap-5 mt-12'
         onSubmit={handleSubmit}>
             <input 
                 type="text"
+                value={searchPrompt}
+                onChange={(e) => setSearchPrompt(e.target.value)}
                 placeholder="Enter product link"
                 className="searchbar-input"
             />
