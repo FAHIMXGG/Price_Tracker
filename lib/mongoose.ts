e4a74bcb2ca2b720d1919f905c4aa0a8c0921326
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2024-08-11 22:38:37 +0600

    mongo fuction setup

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -3,5 +3,19 @@ import mongoose from 'mongoose';
 let isConnected = false; //test connection status
 
 export const connectToDB = async () => {
-    
+    mongoose.set('strictQuery', true);
+
+    if (!process.env.MONGODB_URI) return console.log('MONGODB_URI is not defined')
+
+    if (isConnected) return console.log('connected connection')
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+
+        isConnected = true;
+        console.log('Connected to MongoDB')
+    }
+    catch(error) {
+        console.log(error)
+    }
 }
