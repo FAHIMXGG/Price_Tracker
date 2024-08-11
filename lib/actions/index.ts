e5d8d0b8323682914ee3ebdad9f7c40a575f6db1
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2024-08-11 22:32:33 +0600

    mongoose init

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,11 +1,16 @@
 "use server"
 
+import { connectToDB } from "../mongoose";
 import { ScrapeAmazonProduct } from "../scraper"
 
 export async function ScrapeAndStoreProduct(productUrl: string){
     if (!productUrl) return
 
     try {
+
+        connectToDB();
+
+
         const scrapedProduct = await ScrapeAmazonProduct(productUrl)
         if (!scrapedProduct) return;
     } catch (error: any) {
---
+++ lib/mongoose.ts
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose';
+
+let isConnected = false; //test connection status
+
+export const connectToDB = async () => {
+    
+}
