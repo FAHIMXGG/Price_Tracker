7018e29f59947e4c11a92dffcabc11122785229f
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2024-08-17 12:19:46 +0600

    test sent to db

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,18 +1,44 @@
 "use server"
 
+import { revalidatePath } from "next/cache";
+import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { ScrapeAmazonProduct } from "../scraper"
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 
 export async function ScrapeAndStoreProduct(productUrl: string){
     if (!productUrl) return
 
     try {
+    connectToDB();
+    const scrapedProduct = await ScrapeAmazonProduct(productUrl)
 
-        connectToDB();
+    if (!scrapedProduct) return;
+    let product = scrapedProduct;
+    const exportedProduct = await Product.findOne({ url: scrapedProduct.url })
 
+    if (exportedProduct) {
+        const updatedPriceHistory : any = [
+            ...exportedProduct.priceHistory,
+            {price: scrapedProduct.currentPrice}
+        ]
 
-        const scrapedProduct = await ScrapeAmazonProduct(productUrl)
-        if (!scrapedProduct) return;
+        product = {
+            ...scrapedProduct,
+            priceHistory: updatedPriceHistory,
+            lowestPrice: getLowestPrice(updatedPriceHistory),
+            highestPrice: getHighestPrice(updatedPriceHistory),
+            averagePrice: getAveragePrice(updatedPriceHistory)
+        }
+    }
+    const newProduct = await Product.findOneAndUpdate(
+        { url: scrapedProduct.url },
+         product,
+        { upsert: true, new: true }
+
+    )
+    revalidatePath(`/products/${newProduct._id}`)
+
     } catch (error: any) {
         throw new Error(`Failed to create/update product: ${error.message}`)
     }
