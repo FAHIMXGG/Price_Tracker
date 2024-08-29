4026f52e8cb6993035cb1ede37dbfa4ef5161037
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2024-08-29 20:43:31 +0600

    user track fuctions

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -5,6 +5,7 @@ import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { ScrapeAmazonProduct } from "../scraper"
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+import { User } from "@/types";
 
 export async function ScrapeAndStoreProduct(productUrl: string){
     if (!productUrl) return
@@ -82,6 +83,26 @@ export async function getSimilarProducts(productId: string) {
 
         return  similarProducts;
     } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function addUserEmailToProduct(productId: string, userEmail: string){
+    try{
+        const product = await Product.findById(productId);
+        if(!product) return;
+
+        const userExists = product.users.some((user: User) => user.email === userEmail)
+
+        if (!userExists){
+            product.users.push({email: userEmail})
+            await product.save()
+
+            const emailContent = generateEmailBody(product, 'WELCOME');
+
+        }
+    }
+    catch(error){
         console.log(error)
     }
 }
